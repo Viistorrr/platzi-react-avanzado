1a9d342ff3358d996241a91bdd231c35d0ff835e
viistorrr <victormeza41@gmail.com>
2020-08-09 14:17:38 -0500

    customHooks

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,53 +1,18 @@
 import React, { Fragment, useEffect, useState, useRef } from "react";
 import { Article, ImgWrapper, Img, Button } from "./styles";
 import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { useNearScreen } from "../../hooks/useNearScreen";
 
 let DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
 export const PhotoCard = ({ id, likes = 0, src }) => {
-  const element = useRef(null); //Captura la referencia del elemento en el DOM
-  const [show, setShow] = useState(false);
+  const [show, element] = useNearScreen();
   const key = `like-${id}`;
-  const [liked, setLiked] = useState(() => {
-    try {
-      const like = window.localStorage.getItem(key);
-      return like;
-    } catch (e) {
-      return false;
-    }
-  });
-
-  useEffect(
-    function() {
-      Promise.resolve(
-        typeof window.IntersectionObserver !== "undefined"
-          ? window.IntersectionObserver
-          : import("intersection-observer")
-      ).then(() => {
-        const observer = new window.IntersectionObserver(function(entries) {
-          const { isIntersecting } = entries[0];
-          if (isIntersecting) {
-            setShow(true);
-            observer.disconnect();
-          }
-        });
-        observer.observe(element.current);
-      });
-    },
-    [element]
-  );
+  const [liked, setLiked] = useLocalStorage(key, false);
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder;
-
-  const setLocalStorage = value => {
-    try {
-      window.localStorage.setItem(key, value);
-      setLiked(value);
-    } catch (e) {
-      console.error(e);
-    }
-  };
 
   return (
     <Article ref={element}>
@@ -58,7 +23,7 @@ export const PhotoCard = ({ id, likes = 0, src }) => {
               <Img src={DEFAULT_IMAGE} />
             </ImgWrapper>
           </a>
-          <Button onClick={() => setLocalStorage(!liked)}>
+          <Button onClick={() => setLiked(!liked)}>
             <Icon size="32px" /> {likes} likes!
           </Button>
         </Fragment>
