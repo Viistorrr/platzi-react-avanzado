b37816feb606ab41006f7485b30ee3d9b068fd83
viistorrr <victormeza41@gmail.com>
2020-08-10 17:22:28 -0500

    navbar and protected routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Detail } from "./pages/Detail";
 import { Home } from "./pages/Home";
+import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+import { NotRegisterUser } from "./pages/NotRegisterUser";
+import { NavBar } from "./components/NavBar";
 import Logo from "./components/Logo";
 
 import { Router } from "@reach/router";
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+};
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search);
@@ -17,8 +25,24 @@ export const App = () => {
       <Router>
         <Home path="/" />
         <Home path="/pet/:categoryId" />
-        <Detail path="detail/:detailId" />
+        <Detail path="/detail/:detailId" />
       </Router>
+      <UserLogged>
+        {({ isAuth }) =>
+          isAuth ? (
+            <Router>
+              <Favs path="/favs" />
+              <User path="/user" />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisterUser path="/favs" />
+              <NotRegisterUser path="/user" />
+            </Router>
+          )
+        }
+      </UserLogged>
+      <NavBar />
     </div>
   );
 };
