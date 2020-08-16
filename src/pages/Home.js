808fc411729da87e74f389054567e792b79ed0d7
viistorrr <victormeza41@gmail.com>
2020-08-16 18:50:30 -0500

    react helmet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { ListOfCategories } from "../components/ListOfCategories";
 import { ListOfPhotoCards } from "../container/ListOfPhotoCards";
+import { Layout } from "../components/Layout";
 
 export const Home = ({ categoryId }) => {
   return (
-    <Fragment>
+    <Layout
+      title="Tu App de mascotas"
+      subtitle="Con petgram puedes encontrar fotos de animales domesticos"
+    >
       <ListOfCategories />
       <ListOfPhotoCards categoryId={categoryId} />
-    </Fragment>
+    </Layout>
   );
 };
