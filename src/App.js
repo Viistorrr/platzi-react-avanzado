f529a0b1220328de620486bd9f685bdaae04eabd
viistorrr <victormeza41@gmail.com>
2020-08-16 18:06:04 -0500

    notFound page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Detail } from "./pages/Detail";
 import { Home } from "./pages/Home";
@@ -7,38 +7,32 @@ import { User } from "./pages/User";
 import { NotRegisterUser } from "./pages/NotRegisterUser";
 import { NavBar } from "./components/NavBar";
 import Logo from "./components/Logo";
+import { NotFound } from "./pages/NotFound";
 
-import { Router } from "@reach/router";
-import Context from "./Context";
+import { Router, Redirect } from "@reach/router";
+import { Context } from "./Context";
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search);
-  const detailId = urlParams.get("detail");
+  //const urlParams = new window.URLSearchParams(window.location.search);
+  //const detailId = urlParams.get("detail");
+  const { isAuth } = useContext(Context);
 
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path="/" />
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
+        {!isAuth && <NotRegisterUser path="/login" />}
+        {!isAuth && <Redirect from="/favs" to="/login" />}
+        {!isAuth && <Redirect from="/user" to="/login" />}
+        {isAuth && <Redirect from="/login" to="/" />}
+        <Favs path="/favs" />
+        <User path="/user" />
       </Router>
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <Favs path="/favs" />
-              <User path="/user" />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisterUser path="/favs" />
-              <NotRegisterUser path="/user" />
-            </Router>
-          )
-        }
-      </Context.Consumer>
       <NavBar />
     </div>
   );
