a421d732697245d5f9a61e9e3ba7008859871a96
viistorrr <victormeza41@gmail.com>
2020-08-16 19:26:51 -0500

    react memo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { ListOfCategories } from "../components/ListOfCategories";
 import { ListOfPhotoCards } from "../container/ListOfPhotoCards";
 import { Layout } from "../components/Layout";
 
-export const Home = ({ categoryId }) => {
+const HomePage = ({ categoryId }) => {
   return (
     <Layout
       title="Tu App de mascotas"
@@ -14,3 +14,7 @@ export const Home = ({ categoryId }) => {
     </Layout>
   );
 };
+
+export const Home = React.memo(HomePage, (prevProps, props) => {
+  return prevProps.categoryId === props.categoryId;
+});
