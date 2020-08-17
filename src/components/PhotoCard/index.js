e725c863f9843da7253078046f1d388d09e90563
viistorrr <victormeza41@gmail.com>
2020-08-16 21:10:44 -0500

    propTypes para validaciones de los tipos las props de la App

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -4,6 +4,7 @@ import { FavButton } from "../FavButton";
 import { ToggleLikeMutation } from "../../container/ToggleLikeMutation";
 import { useNearScreen } from "../../hooks/useNearScreen";
 import { Link } from "@reach/router";
+import PropTypes from "prop-types";
 
 let DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
@@ -38,4 +39,20 @@ export const PhotoCard = ({ id, liked, likes = 0, src }) => {
       )}
     </Article>
   );
+};
+
+Photocard.propTypes = {
+  id: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+  likes: function(props, propName, componentName) {
+    const propValue = props[propName];
+    if (propValue === undefined) {
+      return new Error(`${propName} Value must be defined`);
+    }
+
+    if (propValue < 0) {
+      return new Error(`${propName} Value must be greater than 0`);
+    }
+  }
 };
